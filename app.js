f0d174363534b89d660f168d54595212f6a7eb50
Zetian_Chang <Zetian_Chang@trendmicro.com>
2017-10-13 18:31:53 +0800

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');
 
 var routes = require('./routes/index');
 var settings = require('./settings')
@@ -16,6 +17,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash())
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,15 +27,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret:settings.cookieParser,
-  key:settings.db,//cookie name
-  cookie:{maxAge:1000 * 60 * 60 * 24 * 30},
-  store:new MongoStore({
-    db:settings.db,
-    host:settings.host,
-    port:settings.port
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    // db: settings.db,
+    // host: settings.host,
+    // port: settings.port
+    url: 'mongodb://localhost/blog'
   })
-}))
+}));
 
 app.use(routes.BaseUrl,routes.BaseRouter)
 
--- routes/blog.js
+++ routes/blog.js
@@ -10,7 +10,43 @@ router.get('/reg',function(req,res){
 })
 
 router.post('/reg',function(req,res){
-    
+    var name = req.body.name,
+        password = req.body.password,
+        password_re = req.body['password-repeat'];
+    //检验用户两次输入的密码是否一致
+    if (password_re != password) {
+    req.flash('error', '两次输入的密码不一致!'); 
+    return res.redirect('/reg');//返回注册页
+    }
+    //生成密码的 md5 值
+    var md5 = crypto.createHash('md5'),
+        password = md5.update(req.body.password).digest('hex');
+    var newUser = new User({
+        name: name,
+        password: password,
+        email: req.body.email
+    });
+    //检查用户名是否已经存在 
+    User.get(newUser.name, function (err, user) {
+    if (err) {
+        req.flash('error', err);
+        return res.redirect('/');
+    }
+    if (user) {
+        req.flash('error', '用户已存在!');
+        return res.redirect('/reg');//返回注册页
+    }
+    //如果不存在则新增用户
+    newUser.save(function (err, user) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/reg');//注册失败返回主册页
+        }
+        req.session.user = user;//用户信息存入 session
+        req.flash('success', '注册成功!');
+        res.redirect('/');//注册成功后返回主页
+    });
+    });
 })
 
 router.get('/login',function(req,res){
