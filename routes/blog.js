2479ebc723559e53201c22dfc570eefaba2173d8
Zetian_Chang <Zetian_Chang@trendmicro.com>
2017-10-16 14:57:35 +0800

    update

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var crypto = require('crypto'),
+    User = require('../models/user.js');
+
 router.get('/',function(req,res){
     res.render('index',{title:'Express'});
 })
